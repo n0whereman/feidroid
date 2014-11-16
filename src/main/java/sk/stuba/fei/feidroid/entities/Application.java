98a63dcd1b73867b52244f641428a1009b464dc6
bibo <bibaso@gmail.com>
2014-11-16 17:37:29 +0100

    Created project structure and basic api
    Project configured to JPA and JAX-WS with simple api
    to list applications

---
+++ src/main/java/sk/stuba/fei/feidroid/entities/Application.java
@@ -0,0 +1,53 @@
+package sk.stuba.fei.feidroid.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Application {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String description;
+	private String version;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	} 
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getVersion() {
+		return version;
+	}
+	
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	
+	@Override
+	public String toString() {
+		return "Application [name=" + name + ", description=" + description
+        + ", version="+ version +"]";
+	}
+	
+	
+}
---
+++ src/main/java/sk/stuba/fei/feidroid/services/ApplicationService.java
@@ -0,0 +1,54 @@
+package sk.stuba.fei.feidroid.services;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import sk.stuba.fei.feidroid.entities.Application;
+
+@Path("/application")
+public class ApplicationService {
+	private static final String PERSISTENCE_UNIT_NAME = "applications";
+	
+	@GET
+	@Produces("application/json")
+	public Response listAllApps() throws JSONException {
+		
+		JSONArray jsonArray = new JSONArray();
+		JSONObject jsonObject;  
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    EntityManager em = factory.createEntityManager();
+    
+    Query q = em.createQuery("select a from Application a");
+    List<Application> appList =  q.getResultList();
+    
+    for (Application app : appList) {
+      	jsonObject = new JSONObject();
+      	jsonObject.put("id", app.getId());
+      	jsonObject.put("name", app.getName());
+      	jsonObject.put("description", app.getDescription());
+      	jsonObject.put("version", app.getVersion());
+      	
+      	jsonArray.put(jsonObject);
+    }
+
+    String result = jsonArray.toString();
+
+    em.close();
+		
+		return Response.status(200).entity(result).build();
+	}
+}
